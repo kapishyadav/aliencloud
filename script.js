44f2571796a3ca7b55115e693dbdacce2195a6f6
Kapish Yadav <kapishyadav1990@gmail.com>
2025-05-02 20:15:17 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thanks for reaching out! I will get back to you soon.');
+  });
+  
